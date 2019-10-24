c5dce70c7d297bb7276debbddc15e789325e68ed
Pysched <48335713+Pysched@users.noreply.github.com>
2019-10-24 12:20:09 +0100

    Added JavaScript folder and file
    Added javascript folder with index.js file

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+// Back to top function - 
+// Modified from a tutorial found at
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
+
+// Declare the button
+var backToTop = document.getElementById("topBtn");
+
+// When scrolling beyond 50px from the top of the page, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	if(document.body.scroolTop > 50 || document.documentElement.scroolTop > 50) {
+		topBtn.style.display = "block";
+	}	else {
+		topBtn.style.display = "none";
+	}
+}
+
+// Function that makes the page scroll back to the top when pressed
+function topfunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
