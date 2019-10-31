042cffb4bb8f483779021c50b20b359c208d5e49
Pysched <48335713+Pysched@users.noreply.github.com>
2019-10-31 03:56:29 +0000

    Updated index.js
    added chart function

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,3 +21,52 @@ function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+
+// Pie Chart Used in Skills Section
+// Modified from a tutorial located at 
+// https://canvasjs.com/javascript-charts/pie-chart-legends/ - by Canvasjs
+
+// Declare the chart variable
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	exportEnabled: true,
+	animationEnabled: true,
+	title:{
+		// Chart Title
+		text: "Coding Skills"
+	},
+	legend:{
+		// Cursor Type
+		cursor: "pointer",
+		itemclick: explodePie
+	},
+	data: [{
+		// Chart Type
+		type: "pie",
+		showInLegend: true,
+		toolTipContent: "{name}: <strong>{y}%</strong>",
+		indexLabel: "{name} - {y}%",
+		dataPoints: [
+			{ y: 26, name: "HTML5", exploded: true },
+			{ y: 20, name: "CSS3" },
+			{ y: 5, name: "JavaScript" },
+			{ y: 3, name: "Python" },
+			{ y: 7, name: "MYSQLI" },
+			{ y: 17, name: "PHP" },
+			{ y: 22, name: "Jquery"}
+		]
+	}]
+});
+chart.render();
+}
+
+function explodePie (e) {
+	if(typeof (e.dataSeries.dataPoints[e.dataPointIndex].exploded) === "undefined" || !e.dataSeries.dataPoints[e.dataPointIndex].exploded) {
+		e.dataSeries.dataPoints[e.dataPointIndex].exploded = true;
+	} else {
+		e.dataSeries.dataPoints[e.dataPointIndex].exploded = false;
+	}
+	e.chart.render();
+
+}
